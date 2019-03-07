35a711b3437446b819f699605228d1198bc46903
Vatsal <vatsalkgor@gmail.com>
2019-03-07 14:41:15 +0530

    login register with database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,16 +1,16 @@
 import os
-
-from flask import Flask, url_for, render_template, request, redirect, session, make_response
-
+import hashlib
+from flask import Flask, url_for, render_template, request, redirect, session, make_response,flash
+from flask_pymongo import PyMongo
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
+    app.config['MONGO_URI'] = "mongodb://vatsal_new:vatsal123@ds161335.mlab.com:61335/wittybot"
+    app.secret_key="!@#sdfjgh"
+    mongo = PyMongo(app)
+    user = mongo.db.wittybot_users
+    # print((len(user)))
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -56,26 +56,29 @@ def create_app(test_config=None):
                 return redirect('/dashboard')
             else:
             # login logic -- in future will be replaced with database login logic. 
-                if request.form['username'] == "vatsal" and request.form['password'] == "vatsal@223":
+                hashed_pass = hashlib.md5(request.form['password'].encode()).hexdigest()
+                if user.find_one({"username":request.form['username'],"password":hashed_pass}) is not None:
                     session['username'] = request.form['username'];
+                    session['uoid'] = str(user.find_one({"username":request.form['username']})['_id'])
                     return redirect('dashboard')
                 else:
                     return render_template('login.html.j2',error_msg='Username or password is incorrect.')
         else:
-            return redirect('/login')
+            return render_template("login.html.j2")
 
     @app.route('/dashboard')
     def dashboard():
         if 'username' in session:
             # get the all created bots by user and send it to the dashboard
-            return render_template('dashboard.html.j2',bots=None)
+
+            return render_template('dashboard.html.j2',bots=None,name=session['username'])
         else:
             return redirect('/')
 
     @app.route('/logout')
     def logout():
         # remove session here and then call login again
-        session.pop('username')
+        session.clear()
         return redirect('/')
 
     @app.route('/mybots')
@@ -97,8 +100,26 @@ def create_app(test_config=None):
             return render_template('chatbot.html.j2')
         return redirect('/')
         
-    @app.route('/register')
+    @app.route('/register',methods=["GET","POST"])
     def register():
-        return render_template('register.html.j2')
-        
+        if request.method == "POST":
+            if user.find_one({"email":request.form['email']}) is None:
+                if user.find_one({'username':request.form['username']}) is None:
+                    if request.form['password'] == request.form['cpass']:
+                        hash_pass = hashlib.md5(request.form['password'].encode()).hexdigest()
+                        user_details = {"username":request.form['username'],
+                                        "password":hash_pass,
+                                        "email":request.form['email']}
+                        user.insert_one(user_details)
+                        flash("Successfully registered.")
+                        return redirect(url_for("login"))
+                    else:
+                        render_template('register.html.j2',fail_msg="Password and Confirm Password does not match")
+                else:
+                    return render_template("register.html.j2",fail_msg="Choose a different username. ")
+            else:
+                return render_template("register.html.j2",fail_msg="User already registered.")
+        else:
+            return render_template('register.html.j2')
+
     return app
